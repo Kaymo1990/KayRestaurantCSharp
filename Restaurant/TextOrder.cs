4de0eb88a355c2718848dcc475760e77b016bf62
Kay <khurrammohammed1990@gmail.com>
2019-10-09 14:31:07 +0100

    updated twilio method

--- Restaurant/TextOrder.cs
+++ Restaurant/TextOrder.cs
@@ -15,15 +15,15 @@ namespace Restaurant
             {
                 // Find your Account Sid and Token at twilio.com/console
                 // DANGER! This is insecure. See http://twil.io/secure
-                const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-                const string authToken = "your_auth_token";
+                const string accountSid = "AC13146fc0948c7b0a80620342784fb7b4";
+                const string authToken = "38f00c1ba8e62fcd075a8efe2583eb0c";
 
                 TwilioClient.Init(accountSid, authToken);
 
                 var message = MessageResource.Create(
                     body: receipt,
-                    from: new Twilio.Types.PhoneNumber("+15017122661"),
-                    to: new Twilio.Types.PhoneNumber("+15558675310")
+                    from: new Twilio.Types.PhoneNumber("+441622321259"),
+                    to: new Twilio.Types.PhoneNumber("+44787628699")
                 );
 
                 Console.WriteLine(message.Sid);
