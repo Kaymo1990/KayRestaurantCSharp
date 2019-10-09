87ed23fc71e40101c744fc4d659a7389b4cc8017
Kay <khurrammohammed1990@gmail.com>
2019-10-09 12:34:29 +0100

    updated order with order class

--- Restaurant/Order.cs
+++ Restaurant/Order.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Restaurant
 {
-    class Order
+    public class Order
     {
+
+        private IDictionary<string, int> currentOrder = new Dictionary<string, int>();
+
+        public IDictionary<string, int> CurrentOrder { get => currentOrder; set => currentOrder = value; }
+
+        public void addOrder(string item, int quantity)
+        {
+            if (CurrentOrder.ContainsKey(item))
+            {
+                CurrentOrder[item] += quantity;
+            }
+            else
+            {
+                CurrentOrder.Add(item, quantity);
+            }
+            
+        }
+
     }
 }
